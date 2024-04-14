9a08edb9a2993e41245cca3a4e4856b69dd85a10
Alexander De Battista Kvamme <alexanderdebattistakvamme@gmail.com>
2024-04-14 20:56:55 +0200

    Added priceHistory and category tables to drizzle DB

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -6,6 +6,24 @@ export const alkis = pgTable("alkis",{
     price: integer("price").notNull(),
 })
 
+export const category = pgTable("category",{
+    id: integer("id").primaryKey(),
+    categoryName: text("categoryName").notNull(),
+})
+
+export const alkisCategory = pgTable("alkisCategory",{
+    id: integer("id").primaryKey(),
+    alkisId: integer("alkisId").references(() => alkis.id).notNull(),
+    categoryId: integer("categoryId").references(() => category.id).notNull(),
+})
+
+export const priceHistory = pgTable("priceHistory", {
+    id: integer("id").primaryKey(),
+    alkisId: integer("alkisId").references(() => alkis.id).notNull(),
+    priceKroner: integer("priceKroner").notNull(),
+    priceOre: integer("priceOre").notNull(),
+})
+
 export const rawProduct = pgTable("rawProduct",{
     id: integer("id").primaryKey(),
     html: text("html").notNull(),
