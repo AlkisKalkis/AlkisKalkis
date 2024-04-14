2d3aeafa869fcaedfe47a58dbaec570ce92d331e
Petter Charles Redfern <chaaarles@Petters-MacBook-Pro-3.local>
2024-04-14 18:28:09 +0200

    Set up DB connection

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import {Config} from "drizzle-kit";
+
+export default {
+    schema: "./src/db/schema.ts",
+    out: "./drizzle",
+    driver: "pg",
+    dbCredentials: {
+        connectionString: process.env.NEON_DATABASE_URL!,
+    },
+} satisfies Config;
