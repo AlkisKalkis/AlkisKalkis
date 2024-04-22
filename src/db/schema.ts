f8acde62fc08ae0815a0c923890fe7b8d94890fe
Petter Charles Stellberg Redfern <petter.charles@redfern.no>
2024-04-22 17:20:01 +0200

    Update schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,30 +1,37 @@
-import {integer, pgTable, text} from "drizzle-orm/pg-core";
+import { integer, pgTable, text, bigint, boolean } from "drizzle-orm/pg-core";
 
-export const alkis = pgTable("alkis",{
-    id: integer("id").primaryKey(),
+export const alkis = pgTable("alkis", {
+    id: bigint("id", { mode: "bigint" }).primaryKey(),
     name: text("name").notNull(),
     alcoholByVolume: integer("alcoholByVolume").notNull(),
 })
 
-export const category = pgTable("category",{
-    id: integer("id").primaryKey(),
+export const category = pgTable("category", {
+    id: bigint("id", { mode: "bigint" }).primaryKey(),
     categoryName: text("categoryName").notNull(),
 })
 
-export const alkisCategory = pgTable("alkisCategory",{
-    id: integer("id").primaryKey(),
+export const alkisCategory = pgTable("alkisCategory", {
+    id: bigint("id", { mode: "bigint" }).primaryKey(),
     alkisId: integer("alkisId").references(() => alkis.id).notNull(),
     categoryId: integer("categoryId").references(() => category.id).notNull(),
 })
 
 export const priceHistory = pgTable("priceHistory", {
-    id: integer("id").primaryKey(),
+    id: bigint("id", { mode: "bigint" }).primaryKey(),
     alkisId: integer("alkisId").references(() => alkis.id).notNull(),
     priceKroner: integer("priceKroner").notNull(),
     priceOre: integer("priceOre").notNull(),
 })
 
-export const rawProduct = pgTable("rawProduct",{
-    id: integer("id").primaryKey(),
+export const rawProduct = pgTable("rawProduct", {
+    id: bigint("id", { mode: "bigint" }).primaryKey(),
     html: text("html").notNull(),
+})
+
+export const links = pgTable("links", {
+    id: bigint("id", { mode: "bigint" }).primaryKey(),
+    loc: text("loc").notNull(),
+    image: text("image").notNull(),
+    crawled: boolean("crawled").notNull(),
 })
