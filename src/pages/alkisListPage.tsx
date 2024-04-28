ef7767684dd515ca976f1a9671f4ccb23e523ba8
Alexander De Battista Kvamme <alexanderdebattistakvamme@gmail.com>
2024-04-28 18:35:42 +0200

    Loading animation

--- src/pages/alkisListPage.tsx
+++ src/pages/alkisListPage.tsx
@@ -50,7 +50,16 @@ export const AlkisListPage = (): JSX.Element => {
       </nav>
       <main>
         <div class="h-16"></div>
-        {AlkisList({})}
+        <div id="alkis-list">
+          {AlkisList({})}
+        </div>
+        <svg id="spinner" width="100" height="100" viewbox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="20" cy="20" fill="none" r="10" stroke="#383a36" stroke-width="2">
+            <animate attributeName="r" from="8" to="20" dur="1.5s" begin="0s" repeatCount="indefinite"/>
+            <animate attributeName="opacity" from="1" to="0" dur="1.5s" begin="0s" repeatCount="indefinite"/>
+          </circle>
+        </svg>
+
       </main>
     </div>
   );
