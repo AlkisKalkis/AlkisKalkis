28fe6be69eb159ada8ff9ec6d1bb60ae579f858d
Petter Charles Stellberg Redfern <petter.charles@redfern.no>
2024-05-03 13:23:35 +0200

    Add alpinejs script tag

--- src/pages/basePage.tsx
+++ src/pages/basePage.tsx
@@ -3,9 +3,11 @@ export const BasePage = (body: JSX.Element): JSX.Element => {
     <html lang="en">
       <head>
         <title>Hello World</title>
+        <style>[x-cloak] {`{ display: none !important;}`}</style>
         <link type="text/css" rel="stylesheet" href="../public/output.css" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <script src="https://unpkg.com/htmx.org@1.9.12"></script>
+        <script src="https://unpkg.com/htmx.org@1.9.12" defer></script>
+        <script src="https://unpkg.com/alpinejs" defer></script>
       </head>
       <body>{body}</body>
     </html>
